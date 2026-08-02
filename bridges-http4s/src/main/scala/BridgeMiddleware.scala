5faf57ef56fd2af83121848f04f30313dbcb2e0c
Steve Rinn <rinnsteve@gmail.com>
2026-08-02 12:01:16 -0500

    added default values to be set with middleware

--- bridges-http4s/src/main/scala/BridgeMiddleware.scala
+++ bridges-http4s/src/main/scala/BridgeMiddleware.scala
@@ -1,22 +1,29 @@
 import cats.effect.IO
-import contextStorage.IOStorage
 import java.util.UUID
 import logger.BridgeLogger
-import org.http4s.{Header, HttpApp, HttpRoutes}
+import org.http4s.Header
+import org.http4s.HttpApp
 import org.typelevel.ci.CIStringSyntax
 
 object BridgeMiddleware {
-  def apply(logger: BridgeLogger): HttpApp[IO] => HttpApp[IO] =
+  def apply(logger: BridgeLogger, defaultValues: Map[String, String]): HttpApp[IO] => HttpApp[IO] =
     app =>
       HttpApp { request =>
         val correlationId =
-          request.headers.get(ci"X-Correlation-ID").map(_.head.value)
+          request.headers
+            .get(ci"X-Correlation-ID")
+            .map(_.head.value)
             .getOrElse(UUID.randomUUID().toString)
 
         val correlationHeader = Header.Raw(ci"X-Correlation-ID", correlationId)
 
         val requestId = UUID.randomUUID().toString
-        logger.withRequest(correlationId = correlationId, requestId = requestId)( app(request))
+        logger
+          .withRequest(
+            values = defaultValues,
+            correlationId = correlationId,
+            requestId = requestId,
+          )(app(request))
           .map(_.putHeaders(correlationHeader))
       }
 }
